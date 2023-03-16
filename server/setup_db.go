256225a4367670c57c73f0d7d1c9b32350d9a6f3
Edson Michaque <edson@michaque.com>
2023-03-16 11:52:32 +0200

    Setup database

--- server/setup_db.go
+++ server/setup_db.go
@@ -21,15 +21,15 @@ func setupDB(cfg *config.DB) (*database.DB, error) {
 	}
 
 	if cfg.SQLite != nil {
-		return sqlite.New(cfg.Postgres.GetDSN())
+		return sqlite.New(cfg.SQLite.GetDSN())
 	}
 
 	if cfg.MySQL != nil {
-		return mysql.New(cfg.Postgres.GetDSN())
+		return mysql.New(cfg.MySQL.GetDSN())
 	}
 
 	if cfg.MariaDB != nil {
-		return mariadb.New(cfg.Postgres.GetDSN())
+		return mariadb.New(cfg.MariaDB.GetDSN())
 	}
 
 	return nil, errors.New("error")

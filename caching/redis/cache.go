509357811abaefee1082d45d6dc68ea7b073ec26
Edson Michaque <edson@michaque.com>
2023-03-20 23:23:44 +0200

    Implement redis cache

--- caching/redis/cache.go
+++ caching/redis/cache.go
@@ -1,6 +1,12 @@
 package redis
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
 
 func NewCache(rdb *redis.Client) *Cache {
 	return &Cache{
@@ -10,6 +16,14 @@ func NewCache(rdb *redis.Client) *Cache {
 
 type Cache struct {
 	client *redis.Client
+}
+
+func (c Cache) Get(ctx context.Context, key string) (string, error) {
+	return c.client.Get(ctx, key).Result()
+}
+
+func (c Cache) Set(ctx context.Context, key string, value string, dur time.Duration) error {
+	return c.client.Set(ctx, key, value, dur).Err()
 }
 
 func (c Cache) Close() error {

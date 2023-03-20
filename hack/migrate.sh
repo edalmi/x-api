ff73636f12905f04bac97ee774f11dd36f38786c
Edson Michaque <edson@michaque.com>
2023-03-20 23:41:26 +0200

    Validate input for hack/migrate.sh

--- hack/migrate.sh
+++ hack/migrate.sh
@@ -1,3 +1,28 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-migrate create -ext sql -dir "database/$1/migrations" "$2"
+do_migrate() {
+    migrate create -ext sql -dir "database/$1/migrations" "$2"
+}
+
+usage() {
+    echo "Usage:"
+    exit 1
+}
+
+case "$1" in
+    postgres)
+        do_migrate postgres "$2"
+        ;;
+    sqlite)
+        do_migrate sqlite "$2"
+        ;;
+    mysql)
+        do_migrate mysql "$2"
+        ;;
+    mariadb)
+        do_migrate mariadb "$2"
+        ;;
+    *)
+        usage
+        ;;
+esac

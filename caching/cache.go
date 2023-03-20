4f0cb773c6519e07752a08db9b5909ff3d5979f0
Edson Michaque <edson@michaque.com>
2023-03-20 23:23:27 +0200

    DEfine Cache interface

--- caching/cache.go
+++ caching/cache.go
@@ -1,3 +1,11 @@
 package caching
 
-type Cache interface{}
+import (
+	"context"
+	"time"
+)
+
+type Cache interface {
+	Get(ctx context.Context, key string) (string, error)
+	Set(ctx context.Context, key string, value string, dur time.Duration) error
+}
